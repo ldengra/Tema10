1659fa4878c7fb3dd1158b13deb2903d346417b1
ldengra <hiker_luis@hotmail.com>
2020-02-21 10:34:10 +0100

    Ejercicio03

--- src/main/java/com/luisdengra/tema10/Ejercicio03/Main.java
+++ src/main/java/com/luisdengra/tema10/Ejercicio03/Main.java
@@ -3,6 +3,9 @@ package com.luisdengra.tema10.Ejercicio03;
 import java.util.Stack;
 
 public class Main {
+    public static void main(String[] args) {
+     PilaArrayList miPila = new PilaArrayList();
 
-
+     miPila.push();
+    }
 }
--- src/main/java/com/luisdengra/tema10/Ejercicio03/PilaArrayList.java
+++ src/main/java/com/luisdengra/tema10/Ejercicio03/PilaArrayList.java
@@ -4,46 +4,56 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Stack;
 
-public class PilaArrayList extends Stack, List implements IPila{
-
-    private int contador; //numero elementos
-    private ArrayList<String> pila = new ArrayList();
-    //constructor
+public class PilaArrayList implements IPila{
 
 
+    private ArrayList<String> pila = new ArrayList<>();
 
-
-    /**
-     *
-     * @param size es el parametro entero que queremos que mida el array, cuando creamos el objeto, le pasamos como
-     *             parametro el size
-     */
-    public PilaArrayList(int size) {
-        contador = 0;
-    }
     @Override
     public Object push(Object e) {
-
         return null;
     }
 
     @Override
-    public Object pop() {
-        return null;
+    /**
+     * Este metodo es para usar el ultimo dato de la lista, vemos si no esta vacio y usamos el size-1 para la ultima
+     * de la pila que estamos gastando, nos devuelve el dato que hay dentro de la lista y lo eliminamos
+     */
+    public String pop(){
+        String dato = "";
+        if(!isEmpty()){
+            dato=pila.get(pila.size()-1);
+            pila.remove(pila.size()-1);
+        }
+        return dato;
     }
 
     @Override
-    public int size() {
-        return 0;
+    /**
+     * Metodo para ver el tamaño de la lista.
+     */
+    public int size(){
+        return pila.size();
+    }
+
+
+    @Override
+    /**
+     * metodo para ver el ultimo elemento de la lista introducido SIN borrarlo
+     */
+    public String top() {
+        String dato = "";
+        if(!isEmpty()){
+            dato=pila.get(pila.size()-1);
+        }
+        return dato;
     }
 
     @Override
-    public Object top() {
-        return null;
-    }
-
-    @Override
+    /**
+     * Boolean para ver si la lista esta vacia, si el size es 0, es que no hay ningun elemento
+     */
     public boolean isEmpty() {
-        return false;
+        return pila.size()==0;
     }
 }
